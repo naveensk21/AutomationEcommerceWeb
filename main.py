802577368b60135fb3b4d1fec6a20bfb6bc93ee3
Naveen <naveen.sk21@yahoo.com>
2023-09-26 13:22:42 +0200

    Update main

--- main.py
+++ main.py
@@ -6,15 +6,4 @@ with AmazonTest() as bot:
     # bot.search()
     bot.add_to_cart()
 
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# import time
-#
-#
-# driver = webdriver.Firefox()
-# driver.get('https://demoqa.com/automation-practice-form')
-#
-# dob = '29 Sep 2023'
-# date = driver.find_element(By.ID, 'dateOfBirthInput')
-# date.send_keys('29 Sep 2023')
 
